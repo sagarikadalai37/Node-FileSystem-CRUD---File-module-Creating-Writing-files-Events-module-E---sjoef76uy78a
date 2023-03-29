40ff1e9bc2c870b4735cf9095fa00fe0e7e2c4c4
Sagarika Dalai <96329273+sagarikadalai37@users.noreply.github.com>
2023-03-29 23:36:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
-const fs = require('fs/promises')
+const fs = require('fs/promises');
 
 const myFileWriter = async (fileName, fileContent) => {
-	// write code here
-	// dont chnage function name
-}
+  try {
+    await fs.writeFile(fileName, fileContent);
+    console.log(`${fileName} was written successfully`);
+  } catch (error) {
+    console.error(`Error occurred while writing to ${fileName}: ${error.message}`);
+  }
+};
 
 const myFileReader = async (fileName) => {
-	// write code here
-	// dont chnage function name
-}
-
+  try {
+    const data = await fs.readFile(fileName, 'utf-8');
+    console.log(`${fileName} was read successfully`);
+    return data;
+  } catch (error) {
+    console.error(`Error occurred while reading from ${fileName}: ${error.message}`);
+    return null;
+  }
+};
 
 const myFileUpdater = async (fileName, fileContent) => {
-	// write code here
-	// dont chnage function name
-}
+  try {
+    await fs.appendFile(fileName, fileContent);
+    console.log(`${fileName} was updated successfully`);
+  } catch (error) {
+    console.error(`Error occurred while updating ${fileName}: ${error.message}`);
+  }
+};
 
 const myFileDeleter = async (fileName) => {
-	// write code here
-	// dont chnage function name
-}
+  try {
+    await fs.unlink(fileName);
+    console.log(`${fileName} was deleted successfully`);
+  } catch (error) {
+    console.error(`Error occurred while deleting ${fileName}: ${error.message}`);
+  }
+};
 
-
-
-module.exports = { myFileWriter, myFileUpdater, myFileReader, myFileDeleter }
+module.exports = { myFileWriter, myFileUpdater, myFileReader, myFileDeleter };
